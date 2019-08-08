afa9ec9694895eb775e6ac18f9944ed79800dfdf
Gordon Wrigley <gordon.wrigley@quantumblack.com>
2019-08-08 12:07:41 +0100

    update context mixin

--- src/new_kedro_project/pai_mixin.py
+++ src/new_kedro_project/pai_mixin.py
@@ -7,27 +7,24 @@ from datetime import datetime
 
 class PAIKedroContextMixin:
     def __init__(self, project_path, *args, pai_path=Path("logs/pai"), **kwargs):
-        pai.set_config(
-            experiment=self.pai_experiment, local_path=str(project_path / pai_path)
-        )
-        run_name = "Model Run at %s" % datetime.now().strftime("%H:%M:%S")
-        with pai.start_run(run_name=run_name):
-            self.run_id = pai.current_run_uuid()
-
         super().__init__(project_path, *args, **kwargs)
+
+        pai.set_config(
+            experiment=self.pai_experiment, storage_path=str(project_path / pai_path)
+        )
+
+    def _create_catalog(self, conf_catalog, conf_creds) -> DataCatalog:
+        save_version = generate_current_version()
+
+        run_id = pai.current_run_uuid()
+        if run_id:
+            save_version += "-" + run_id
+
+        return DataCatalog.from_config(
+            conf_catalog, conf_creds, save_version=save_version
+        )
 
     def run(self, *args, **kwargs):
         run_name = "Model Run at %s" % datetime.now().strftime("%H:%M:%S")
-        with pai.start_run(run_id=self.run_id):
+        with pai.start_run(run_name=run_name):
             super().run(*args, **kwargs)
-
-    def _create_catalog(self) -> DataCatalog:
-        save_version = "{}-{}".format(generate_current_version(), self.run_id)
-
-        conf_catalog = self._config_loader.get("catalog*", "catalog*/**")
-        conf_creds = self._get_config_credentials()
-        catalog = DataCatalog.from_config(
-            conf_catalog, conf_creds, save_version=save_version
-        )
-        catalog.add_feed_dict(self._get_feed_dict())
-        return catalog
